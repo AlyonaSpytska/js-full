8f9daae4d7e5ef35d7e19f714d8d7327349b8f98
Alyona Spytska <mshelenspytska@gmail.com>
2021-07-29 20:04:21 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+//                            LOOPS
+
+// Задача:   Умножение нечетных чисел от m до n
+// const m = 10;
+// const n = 20;
+// let result = 1;
+// let y = m;
+
+// for (; y <= n;) {
+//   if (y % 2 === 1) {
+//     result *= y;
+//   }
+//   y++;
+// }
+// console.log(result);
+// console.log(m);
+// console.log(n);
+
+// Задача:   Таблица умножения
+// for (let i = 1; i <= 5; i += 1) {
+//   for(let x = 1; x <= 10; x += 1) {
+//     console.log(i + ' x ' + x +  ' = ' + i * x);
+//   }
+// }
+
+function run() {
+  var a = 0;
+
+  if (a == 1) {
+    return c;
+  }
+
+  for (let i = 1; i < 10; i++) {
+    console.log(i);
+  }
+
+  return (d = a);
+}
+
+run();
